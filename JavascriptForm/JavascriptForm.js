3d001b14203355bb36953cc604b27cefc6a0ece4
Yohann Delacroix <yohann.delacroix@hotmail.fr>
2023-03-25 13:09:48 +0100

    Update JavascriptForm.js

--- JavascriptForm/JavascriptForm.js
+++ JavascriptForm/JavascriptForm.js
@@ -1,11 +1,23 @@
-let feedbackMsg = document.createElement('p');
-feedbackMsg.classList.add("result");
-let txt = document.createTextNode("Results will be here on next time") ;
-feedbackMsg.appendChild(txt);
+const addLineToResult = (name, content) => {
 
-let feedbackNode = document.getElementById("feedbackMsg");
-feedbackNode.appendChild(feedbackMsg);
+   let feedbackMsg = document.createElement('p');
+   feedbackMsg.classList.add("result");
+   let newElem = document.createTextNode(`${name} : ${content}`) 
+   feedbackMsg.appendChild(newElem);
+  
+  let feedbackNode =     document.getElementById("feedbackMsg");
+  feedbackNode.appendChild(feedbackMsg);
+} 
 
-
-
-
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  alert("okc");
+  
+  for( [key, value] of data) {
+    console.log(key, ",", value) ;
+  } 
+  
+  addLineToResult("Name", e.target.name.value);
+  addLineToResult("First name", e.target.firstname.value);
+  addLineToResult("Birth date", e.target.date.value);
