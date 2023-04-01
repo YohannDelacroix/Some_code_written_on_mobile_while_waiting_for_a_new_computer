6f268b049f8ba29e3286bf4f6ca5822ed514ef9b
Yohann Delacroix <yohann.delacroix@hotmail.fr>
2023-04-01 11:01:59 +0200

    Update JavascriptForm.js

--- JavascriptForm/JavascriptForm.js
+++ JavascriptForm/JavascriptForm.js
@@ -1,3 +1,55 @@
+//Add a new custom line to the result section
+const addLineToResult = (name, content) => {
+   let feedbackNode =     document.getElementById("feedbackMsg");
+  
+   //Create the new block with datas
+   let feedbackBlock = document.createElement('div');
+  
+  
+   //Create the description block left
+   let feedbackDesc = document.createElement('h5');
+   let newDesc = document.createTextNode(`${name}`);
+   feedbackDesc.appendChild(newDesc);
+   
+   feedbackNode.appendChild(feedbackDesc);
+  
+  
+   //Create the content block right
+   let feedbackMsg = document.createElement('p');
+   feedbackMsg.classList.add("result");
+   let newElem = document.createTextNode(`${content}`) 
+   feedbackMsg.appendChild(newElem);
+   feedbackNode.appendChild(feedbackMsg);
+}
+
+//Validate the form if all the fields are completed
+const validationControl = (formData) => {
+  let validation = true;
+  
+  for( [key, value] of formData) {
+    console.log("key", key) 
+    console.log("val", value) 
+    
+    let inputNode = document.getElementById(`${key}`);
+    
+    //delete the existing error message if exists
+    let errorMsgNode = document.getElementById("errorMsg"+key);
+    if(errorMsgNode) {
+        errorMsgNode.remove();
+    } 
+      
+    //when a param is missing
+    if(!formData.get(key))
+    {
+       console.log("Parameter missing :", key);
+       
+       if(!inputNode) {
+         console.log("input node null") 
+         return;
+       } 
+      
+       //Add error class
+       inputNode.classList.add("errorInput");
       
        //Display an error message onto screen
        let errorMsg = document.createElement("p");
@@ -7,18 +59,17 @@
        errorMsg.appendChild(errorTxt);
        inputNode.after(errorMsg);
       
-      
        validation = false;
     } 
     else {
       inputNode.classList.remove("errorInput");
-      
     } 
    } 
   
    return validation;
-} 
+}
 
+//When submit button is pressed
 const handleSubmit = (e) => {
   e.preventDefault();
   
@@ -33,3 +84,10 @@ const handleSubmit = (e) => {
   addLineToResult("Birth date", data.get("date"));
   } 
 } 
+
+
+let form = document.getElementById("form");
+form.addEventListener("submit", handleSubmit);
+
+
+
